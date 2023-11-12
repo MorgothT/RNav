592a203e6eee9611d3d6e26af30a4f97176020eb
Tal Shachar <Tal.Shachar@edtmc.com>
2023-11-12 11:01:19 +0200

    111

---
+++ Models/Target.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Mapsui;
+using System.Windows;
+
+namespace Mapper_v1.Models;
+
+public partial class Target : ObservableObject
+{
+	[ObservableProperty]
+	private string name = "";
+	[ObservableProperty]
+	private double x;
+    [ObservableProperty]
+    private double y;
+    [ObservableProperty]
+    private double lat;
+    [ObservableProperty]
+    private double lon;
+    [ObservableProperty]
+    private double radius = 1.0;
+
+    /// <summary>
+    /// Target point (unaware of EPSG !)
+    /// </summary>
+    public Target()
+    {
+    }
+
+    public override string ToString()
+    {
+        return $"{Name},{X},{Y},{Lat},{Lon},{Radius}";    
+    }
+}
